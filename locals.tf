e0798f0596e5c836fe71ba790daddf284d88c1e8
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-18 10:53:11 +0530

    cc

--- locals.tf
+++ locals.tf
@@ -7,9 +7,9 @@ locals{
                       #  ["10.0.1.0/24","10.0.2.0/24"]
     backend_alb_listener-arn = data.aws_ssm_parameter.backend_alb_listener-arn.value
     frontend_alb_listener-arn = data.aws_ssm_parameter.frontend_alb_listener-arn.value
-    listener_arn = ${var.comonent} == "frontend" ? local.frontend_alb_listener : local.backend_alb_listener
+    listener_arn = "${var.comonent}" == "frontend" ? local.frontend_alb_listener : local.backend_alb_listener
 
-    tg_port = ${var.comonent_name} == "frontend" ? 80 : 8080  
-    health_check_path =   ${var.comonent_name} == "frontend" ? "/" : "/health"     
-    host_context = ${var.comonent} == "frontend" ? "${var.project_name}-${var.environment}.${domain_name}" : ${var.comonent_name}.backend-alb-${var.environment}.${var.domain_name}        
+    tg_port = "${var.comonent_name}" == "frontend" ? 80 : 8080  
+    health_check_path =   "${var.comonent_name}" == "frontend" ? "/" : "/health"     
+    host_context = "${var.comonent}" == "frontend" ? "${var.project_name}-${var.environment}.${domain_name}" : "${var.comonent_name}.backend-alb-${var.environment}.${var.domain_name}"        
 }
